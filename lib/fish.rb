3f3588cccc8b3d26358a38f39b35ca11fbd98b51
Samuel Prada <jspradac@gmail.com>
2018-11-29 21:30:34 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,20 @@
 class Fish
+  attr_reader :name 
+  attr_accessor :mood 
+  @@fishes = [] 
+  def initialize(name, mood = "nervous")
+    @name = name 
+    @mood = mood
+    @@fishes << self 
+  end 
+
+  def self.find_fish(name)
+    @@fishes.find { |fish| fish.name == name }
+  end 
+  
+  def self.remove_fish(name)
+    @@fishes - [find_fish(name)]
+  end 
+  
   # code goes here
 end
