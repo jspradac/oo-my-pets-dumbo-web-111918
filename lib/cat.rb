7061fcde366b9bbac2c63b45e710350bd3083252
Samuel Prada <jspradac@gmail.com>
2018-11-29 14:29:57 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
+  attr_reader :name 
+  attr_accessor :mood 
+  @@cats = [] 
+  def initialize(name, mood = "nervous")
+    @name = name 
+    @mood = mood
+    @@cats << self 
+  end 
+
+  def self.find_cat(name)
+    @@cats.find { |cat| cat.name == name }
+  end 
+  
+  def self.remove_cat(name)
+    @@cats - [find_cat(name)]
+  end 
+  
   # code goes here
 end
