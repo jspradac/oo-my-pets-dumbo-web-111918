60bfe2cfe160adb698c75b9cd57d41a39a0b37f0
Samuel Prada <jspradac@gmail.com>
2018-11-30 14:57:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+  attr_reader :name 
+  attr_accessor :mood 
+  @@dogs = [] 
+  def initialize(name, mood = "nervous")
+    @name = name 
+    @mood = mood
+    @@dogs << self 
+  end 
+
+  def self.find_dog(name)
+    @@dogs.find { |dog| dog.name == name }
+  end 
+  
+  def self.remove_dog(name)
+    @@dogs - [find_dog(name)]
+  end 
+  
   # code goes here
 end
